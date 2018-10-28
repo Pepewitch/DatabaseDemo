16b079e0b4b5d02265a91c3251f3c8a4aa040487
Pepe Witch <skpepe134@gmail.com>
2018-10-28 13:16:38 +0700

    add middleware for express app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,2 @@
-import express from "express";
-import { Model } from "./model/model";
-
-const app = express();
-app.get("/", (req, res) => {
-  new Model();
-  return res.status(200).send({ message: "Hello world" });
-});
-app.listen(80)
+import app from './app';
+app.listen(process.env.PORT);
