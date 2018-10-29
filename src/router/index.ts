8b49ddf6eac2404f0799beddcf76bf4539de1a8f
Pepe Witch <skpepe134@gmail.com>
2018-10-29 22:34:56 +0700

    edit GET / to show how express-session work

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,16 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.status(200).send({ message: 'Hello world!' });
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.write('<p>expires in: ' + req.session.cookie.maxAge / 1000 + 's</p>');
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end('welcome to the session demo. refresh!');
+  }
 });
 
 export default router;
