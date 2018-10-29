11a8696b8a8c2bea9fe055a93747e9827dda665d
Pepe Witch <skpepe134@gmail.com>
2018-10-29 22:14:34 +0700

    log port when start server

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ import UserController from './controllers/User/UserController';
 
 UserController.getUsers();
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Start server at port :', process.env.PORT || 3000);
+});
