983b9bc9ef6f2321b9dbdc3d23ae4a0c343367fa
Pepe Witch <skpepe134@gmail.com>
2018-10-30 17:21:20 +0700

    add method to query only 1 user

--- src/controllers/User/UserController.ts
+++ src/controllers/User/UserController.ts
@@ -2,26 +2,23 @@ import Controller from '..';
 import { User } from '../../entity/User';
 import { AsyncErrorHandler } from '../../decorators/error-handler';
 
-interface UserOption extends UserInfo {
-  id?: number;
-}
-
-interface UserInfo {
-  firstName?: string;
-  lastName?: string;
-  age?: number;
-}
-
 export default class UserController {
   @AsyncErrorHandler()
-  public static async getUsers(options?: UserOption) {
+  public static async getUser(username: string) {
+    const connection = await Controller.getConnection();
+    const user = await connection.manager.findOne(User, username);
+    return user;
+  }
+
+  @AsyncErrorHandler()
+  public static async getUsers(options?: Partial<User>) {
     const connection = await Controller.getConnection();
     const users = await connection.manager.find(User, { where: options });
     return users;
   }
 
   @AsyncErrorHandler()
-  public static async addUser(options: UserInfo) {
+  public static async addUser(options: User) {
     const connection = await Controller.getConnection();
     const user = new User();
     Object.assign(user, options);
