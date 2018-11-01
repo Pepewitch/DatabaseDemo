8029b0a92772740e71ab5b0f3869e11a95eeead0
Pepe Witch <skpepe134@gmail.com>
2018-11-01 12:16:41 +0700

    use google strategy

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,21 +1,37 @@
 import passport from 'passport';
 import UserController from '../controllers/User/UserController';
 import { User } from '../entity/User';
-import local from './local';
+import local from './strategy/local';
+import facebook from './strategy/facebook';
+import google from './strategy/google';
+
+export enum AuthenticationType {
+  local,
+  facebook,
+}
 
 passport.serializeUser((user: User, done) => {
-  done(null, user.username);
+  console.log('serialize', user);
+  done(null, user);
 });
 
 passport.deserializeUser(async (username: string, done) => {
+  console.log('deserialize', username);
   try {
     const user = await UserController.getUser(username);
-    done(null, user);
+    console.log('get user', username, user);
+    if (user) {
+      done(null, user);
+    } else {
+      throw new Error('User does not exist');
+    }
   } catch (err) {
     done(err, null);
   }
 });
 
 passport.use(local);
+passport.use(facebook);
+passport.use(google);
 
 export default passport;
