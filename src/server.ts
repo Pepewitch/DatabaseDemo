8bf36631cc65b96473cc6c1e7d6cb6558e29eaad
Pepe Witch <skpepe134@gmail.com>
2018-11-07 18:38:53 +0700

    test connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
+import Controller from './controllers';
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Start server at port :', process.env.PORT || 3000);
+  Controller.testConnection();
 });
