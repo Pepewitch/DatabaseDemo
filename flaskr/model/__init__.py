5438f756f6a2f8c0192d67b36e4fcab2fd6cee21
Pepe Witch <skpepe134@gmail.com>
2018-11-07 22:47:41 +0700

    add getConnection method to connect to the database

---
+++ flaskr/model/__init__.py
@@ -0,0 +1,4 @@
+import pymysql
+
+def getConnection():
+    return pymysql.connect(host='dgo.ojudge.in.th', user='dbsec33', password='123456', db='Hospital', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
