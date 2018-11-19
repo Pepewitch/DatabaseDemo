444cf4fe54c044b5fec7e9beec803e13cd2f4f69
Witchayut <skpepe134@gmail.com>
2018-11-19 20:02:21 +0700

    add cors

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,12 +4,14 @@ from cheroot.wsgi import Server, PathInfoDispatcher
 from flask import Flask, send_file
 from model import staff
 from os.path import join
+from flask_cors import CORS
 from route.medical_staff import medical_staff_api
 from route.appoint import appoint_api
 from route.patient import patient_api
 from route.department import department_api
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(medical_staff_api)
 app.register_blueprint(appoint_api)
 app.register_blueprint(patient_api)
