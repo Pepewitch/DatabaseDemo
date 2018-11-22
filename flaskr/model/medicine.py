2ac2443bb97c9b17f40e1d67defeb8147dd4beba
Natawat Kwanpum <natawat.nueng@gmail.com>
2018-11-22 16:07:25 +0700

    Add medicine model and route (Not perfect version)

--- flaskr/model/medicine.py
+++ flaskr/model/medicine.py
@@ -1,1 +1,145 @@
-# TODO: Add method to manage medicine table
+import pymysql
+from . import getConnection
+
+
+def getMedicine(medicine_id=None):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            query = "SELECT * FROM Medicine"
+            condition = []
+            if medicine_id is not None:
+                condition.append(f'Medicine_ID = {medicine_id}')
+            if len(condition) > 0:
+                query += ' WHERE ' + ' AND '.join(condition)
+            cursor.execute(query)
+            if medicine_id:
+                result = cursor.fetchone()
+            else:
+                result = cursor.fetchall()
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+def edit(
+    medicine_id , 
+    name=None , 
+    quantity=None , 
+    exp_date=None 
+):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            temp = 'UPDATE Medecine SET '
+            query = temp
+    
+            if firstname is not None:
+                query += f'Name = "{name}" , '
+
+            if quantity is not None:
+                query += f'Quantity = "{quantity}" , '
+            if exp_date is not None:
+                query += f'Exp_date = "{exp_date}" , '
+            if query == temp:
+                raise ValueError('Args are invalid!')
+            else:
+                query = query[:-2]
+                query += f' WHERE Medicine_ID = {medicine_id}'
+            cursor.execute(query)
+            mysql.commit()
+    except ValueError as e:
+        result = e
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+def delete(medicine_id):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            query = f"DELETE FROM Medicine WHERE Medicine_ID = {medicine_id}"
+            cursor.execute(query)
+            mysql.commit()
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+def insertMedicine(name,quantity,exp_date):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            query1 = f"INSERT INTO Medicine (\
+	        Name,Quantity,Exp_date\
+            ) VALUES (\
+	        '{name}',\
+            '{quantity}' , \
+            '{exp_date}' , \
+            );"
+            cursor.execute(query1)
+            mysql.commit()
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+def refillMedicine(medicine_id, pharmacist_id,quantity,date):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            query1 = f"INSERT INTO Refill (\
+	        Medicine_ID,Pharmacist_ID, Quantity,Refill_date\
+            ) VALUES (\
+	        '{medicine_id}',\
+            '{pharmacist_id}' , \
+            '{quantity}' , \
+            '{date}', \
+            );"
+            cursor.execute(query1)
+            #Todo : update in medecine table
+            # Date my get from system date time
+            mysql.commit()
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+
+def perscribeMedicine(doctor_id, patient_id,medicine_id, quantity ,date):
+    mysql = getConnection()
+    result = None
+    try:
+        with mysql.cursor() as cursor:
+            query1 = f"INSERT INTO Perscribe (\
+	        Doctor_ID,Patient_ID,Medicine_ID, Perscribe_date, Quantity\
+            ) VALUES (\
+	        '{doctor_id}',\
+            '{patient_id}' , \
+            '{medicine_id}' , \
+            '{date}', \
+            '{quantity}', \
+            );"
+            cursor.execute(query1)
+            #Todo : update in medecine table ???
+
+            mysql.commit()
+    except Exception as e:
+        print (e)
+    finally:
+        mysql.close()
+    return result
+
+
+
---
+++ flaskr/route/medicine.py
@@ -0,0 +1,102 @@
+from flask import Blueprint, request, jsonify
+from model import medicine
+from werkzeug.exceptions import HTTPException
+from dateutil import parser
+
+medicine_api = Blueprint('medicine_api', __name__ , url_prefix='/api')
+
+@medicine_api.route('/medicine' , methods=('GET',))
+def medicine_route():
+    try:
+        params = {
+            'medicine_id' : request.args.get('id')
+        }
+        return jsonify(medicine.getMedicine(**params))
+    except Exception:
+        return '' , 500
+
+@medicine_api.route('/medicine/<medicine_id>' , methods=('GET' , 'PATCH' , 'DELETE'))
+def medicine_id_route(medicine_id):
+    if request.method == 'GET':
+        try:
+            params = {
+                'medicine_id' : medicine_id
+            }
+            return jsonify(medicine.getMedicine(**params))
+        except Exception:
+            return '' , 500
+    elif request.method == 'PATCH':
+        try:
+            params = { 'medicine_id' : medicine_id }
+            keys = ['name' , 'quantity' , 'exp_date']
+            for key in keys:
+                if key in request.form:
+                    params[key] = request.form[key]
+            medicine.edit(**params)
+            return '' , 200
+        except Exception:
+            return '' , 500
+    elif request.method == 'DELETE':
+        try:
+            medicine.delete(medicine_id)
+            return '' , 200
+        except Exception:
+            return '' , 500
+        
+@medicine_api.route('/medicine/refill' , methods=('POST'))
+def medicine_refill_route():
+    print(request.form)
+    # if request.method == 'GET':
+    #     try:
+    #         params = {
+    #             'medical_type' : 'Doctor',
+    #             'staff_id' : None
+    #         }
+    #         return jsonify(staff.getMedicalStaff(**params))
+    #     except Exception:
+    #         return '' , 500
+    if request.method == 'POST':
+        try:
+            params = {
+                'medicine_id': request.form['medicine_id'] ,
+                'pharmacist_id': request.form['pharmacist_id'], 
+                'quantity': request.form['quantity'], 
+                'date': request.form['date'] #TODO: change to use system datetime.
+            }
+            medicine.refilMedicine(**params)
+            return '' , 200
+        except HTTPException:
+            return jsonify({'message' : 'Arguments are invalid'}) , 400
+        except Exception as e:
+            print(e)
+            return '' , 500
+
+@medicine_api.route('/medicine/perscribe' , methods=('POST'))
+def medicine_perscribe_route():
+    print(request.form)
+    # if request.method == 'GET':
+    #     try:
+    #         params = {
+    #             'medical_type' : 'Doctor',
+    #             'staff_id' : None
+    #         }
+    #         return jsonify(staff.getMedicalStaff(**params))
+    #     except Exception:
+    #         return '' , 500
+    if request.method == 'POST':
+        try:
+            params = {
+                'medicine_id': request.form['medicine_id'] ,
+                'patient_id': request.form['patient_id'], 
+                'doctor_id': request.form['doctor_id'], 
+                'date': request.form['date'],  #TODO: change to use system date time
+                'quantity' : request.form['quantity']
+            }
+            medicine.refilMedicine(**params)
+            return '' , 200
+        except HTTPException:
+            return jsonify({'message' : 'Arguments are invalid'}) , 400
+        except Exception as e:
+            print(e)
+            return '' , 500
+
